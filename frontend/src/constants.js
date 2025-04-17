e64786542420de42b396e1c6f796027af2932993
Peter-N-Wainaina <pnw6@cornell.edu>
2025-04-16 23:21:08 -0400

    Allow frontend to access the backend base url once deployed

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -10,6 +10,8 @@ export const FIRST = "first";
 export const SECOND = "second";
 
 
-const BASE_URL = "http://localhost:8000";
+
+
+const BASE_URL = process.env.BACKEND_BASE_URL || "http://localhost:8000";
 export const START_ENDPOINT = `${BASE_URL}/start`;
 export const MAKE_MOVE_ENDPOINT = `${BASE_URL}/make_move`;
